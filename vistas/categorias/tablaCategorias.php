3c1c5cbbe7da8d1d5cf21e55b9890efb18994bce
Leonardo Wiesner <leonardowiesner11@gmail.com>
2022-10-02 20:20:02 +0200

    addcategorias

---
+++ vistas/categorias/tablaCategorias.php
@@ -0,0 +1,21 @@
+<table class= "table table-hover table-condensed table-bordered" style="text-align: center;">
+    <caption> <label>Categorias :</label> </caption>
+<tr>
+        <td>Categoria</td>
+        <td>Editar</td>
+        <td>Eliminar</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>
+            <span class="btn btn-warning btn-sm">
+            <span class="glyphicon glyphicon-pencil"</span>
+            </span>
+        </td>
+        <td>
+        <span class="btn btn-danger btn-sm">
+            <span class="glyphicon glyphicon-remove"</span>
+        </span>
+        </td>
+    </tr>
+</table>
